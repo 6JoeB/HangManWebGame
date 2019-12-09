3e68e6cd7ec96b01b9e3cef489d0832b4dfbc11c
swade <sarah.es@hotmail.co.uk>
2019-12-09 13:41:20 +0000

    pleassse

--- HangManTests/UnitTest1.cs
+++ HangManTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using HangManGame.Models;
 using NUnit.Framework;
 
 namespace HangManTests
@@ -7,12 +8,38 @@ namespace HangManTests
         [SetUp]
         public void Setup()
         {
+           Play play = new Play();
         }
 
         [Test]
-        public void Test1()
+        public void CheckForArray()
         {
-            Assert.Pass();
+            
+            Assert.That(play.words, Is.Not.Empty);
         }
     }
 }
+
+
+
+
+
+/*
+
+        [SetUp]
+public void Setup()
+{
+    bankAccount = new BankAccount();
+}
+// Above setups up and makes bank account for each tests, like let. 
+
+[Test]
+public void BankAccount_HasBalance_Of0()
+{
+    //Arrange 
+    //Act
+    //Assert 
+    Assert.That(bankAccount.Balance == 0);
+}
+
+*/
