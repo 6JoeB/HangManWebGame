ea554e825d178e37b42378b025d331891314b49e
6JoeB <joe6ball@gmail.com>
2019-12-10 14:13:45 +0000

    check guess function updated

--- HangManGame/Models/Game.cs
+++ HangManGame/Models/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace HangManGame.Models
 
@@ -9,38 +10,50 @@ namespace HangManGame.Models
     {
         public string Word { get; set; }
         List<string> LettersAvailable = new List<string>(new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" });
-        List<string> CorrectlyGuessed = new List<string>(new string[] { });
-        List<string> IncorrectlyGuessed = new List<string>(new string[] { });
+        List<string> CorrectlyGuessed = new List<string>();
+        List<string> IncorrectlyGuessed = new List<string>();
         public int NumberOfGuesses;
         public string Guess { get; set; }
-        public bool win;
+        public bool Win;
+        public int MatchingIndex;
+        public int Index;
 
         public void GetGuess(string guess)
         {
             Guess = guess;
         }
 
-        public void CheckGuess(string guess)
+        public void GetWord(string word)
+        {
+            Word = word;
+        }
+
+        public void CheckGuess()
         {
             var splitWord = Word.Split(new string[] { "" }, StringSplitOptions.None);
 
-            if (guess != null) //guess cannot be empty
+            if (Guess != null) //guess cannot be empty
             {
-                if ((!CorrectlyGuessed.Contains(guess)) || (!IncorrectlyGuessed.Contains(guess))) //checks if guess has already been guessed
+                if ((!CorrectlyGuessed.Contains(Guess)) || (!IncorrectlyGuessed.Contains(Guess))) //checks if guess has already been guessed
                 {
-                    NumberOfGuesses += 1;
                     if (splitWord.Contains(Guess)) //checking if word has letter being guessed
-                        {
-                        int index = 
-                        CorrectlyGuessed.Insert(index, guess);
+                        { 
+                            for(int i = 0; i < splitWord.Length; i++)
+                            {
+                                if(splitWord[i] == Guess)
+                                {
+                                    CorrectlyGuessed[i] = Guess;
+                                }
+                            }   
                         }
-                    else if (Word.Contains(guess)) //checking if word guessed is secret word
+                    else if (Word.Contains(Guess)) //checking if word guessed is secret word
                         {
-                        win = true;
+                        Win = true;
                         }
                     else //if guess is not correct
                         {
-                        IncorrectlyGuessed.Append(guess);
+                        NumberOfGuesses += 1;
+                        IncorrectlyGuessed.Append(Guess);
                         }
                         
                 }
