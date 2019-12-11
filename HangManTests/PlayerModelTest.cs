8617d59a1b78c18f4b83f91dc97f819f549ce492
6JoeB <joe6ball@gmail.com>
2019-12-11 10:05:34 +0000

    fixed master

---
+++ HangManTests/PlayerModelTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using HangManGame.Models;
+
+namespace HangManTests
+
+{
+    [TestFixture]
+    class PlayerModelTests
+    {
+        Player player;
+
+        string name = "Steph Curry";
+
+        [SetUp]
+        public void Setup()
+        {
+            player = new Player
+            {
+                Name = name
+            };
+        }
+
+        [Test]
+        public void CreatesAnInstanceOfPlayer()
+        {
+            Assert.That(player, Is.InstanceOf<Player>());
+
+        }
+
+        [Test]
+        public void PlayerHasName()
+        {
+            Assert.AreEqual(player.Name, name);
+        }
+
+        [Test]
+        public void PlayerCanRegister()
+        {
+            Player newPlayer = Player.Register(name);
+            Assert.That(newPlayer, Is.InstanceOf<Player>());
+        }
+    }
+}
