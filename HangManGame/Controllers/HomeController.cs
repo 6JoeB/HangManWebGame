e111cf46b8434aa1e40be21b5f36a82dda02c77d
6JoeB <joe6ball@gmail.com>
2019-12-11 12:55:42 +0000

    temp checking out wor gen branch

--- HangManGame/Controllers/HomeController.cs
+++ HangManGame/Controllers/HomeController.cs
@@ -5,26 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HangManGame.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace HangManGame.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Profile()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your Profile page.";
 
             return View();
         }
