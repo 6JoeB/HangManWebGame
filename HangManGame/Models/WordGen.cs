7c60417adee549eeb5ff08d563fabe8d72fc039d
6JoeB <joe6ball@gmail.com>
2019-12-11 10:24:33 +0000

    word gen merge

---
+++ HangManGame/Models/WordGen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangManGame.Models
+{
+    public class wordGenerator
+    {
+        public static string randomWordGetter()
+        {
+            string[] words = { "red", "green", "blue", "purple", "pink" };
+            Random rand = new Random();
+            int index = rand.Next(words.Length);
+            return words[index];
+        }
+    }
+}
---
+++ HangManTests/UnitTests/WordGenTest.cs
@@ -0,0 +1,25 @@
+﻿using HangManGame.Models;
+using NUnit.Framework;
+
+namespace HangManTests
+{
+    [TestFixture]
+
+    class WordGenTests
+    {
+
+        public wordGenerator wordGenerator;
+        [SetUp]
+        public void Setup()
+        {
+            wordGenerator = new wordGenerator();
+        }
+
+        [Test]
+        public void CheckForArray()
+        {
+            var result = wordGenerator.randomWordGetter();
+            Assert.IsInstanceOf(typeof(string), result);
+        }
+    }
+}
