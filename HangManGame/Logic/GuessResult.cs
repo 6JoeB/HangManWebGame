81ef922eec6c008fa18ed40c97f2157cc96b0f77
6JoeB <joe6ball@gmail.com>
2019-12-17 11:07:44 +0000

    added logic models

---
+++ HangManGame/Logic/GuessResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HangManGame.Logic
+{
+    public class GuessResult
+    {
+        public GuessResult(bool hasWon, bool hasLost, List<char> correctGuesses, List<char> incorrectGuesses, int livesRemaining)
+        {
+            HasWon = hasWon;
+            HasLost = hasLost;
+            CorrectGuesses = correctGuesses;
+            IncorrectGuesses = incorrectGuesses;
+            LivesRemaining = livesRemaining;
+        }
+
+        public bool HasWon { get; private set; }
+        public bool HasLost { get; private set; }
+
+        public List<char> CorrectGuesses { get; private set; }
+        public List<char> IncorrectGuesses { get; private set; }
+        public int LivesRemaining { get; private set; }
+    }
+}
---
+++ HangManGame/Models/Guesses.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using HangManGame.Logic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace HangManGame.Models
+{
+    public class Guesses
+    {
+        private readonly string word;
+        private char[] allLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
+
+        public Guesses() { }
+        public Guesses(string word)
+        {
+            this.word = word;
+            LettersAvailable = allLetters.ToList();
+        }
+
+        public Guesses(string word, char? guess, GuessResult result)
+        {
+            this.word = word;
+            Guess = guess;
+            CorrectGuesses = result.CorrectGuesses;
+            IncorrectGuesses = result.IncorrectGuesses;
+            HasWon = result.HasWon;
+            HasLost = result.HasLost;
+            LivesRemaining = result.LivesRemaining;
+
+            PreviousGuesses = new List<char>();
+            PreviousGuesses.AddRange(IncorrectGuesses);
+            PreviousGuesses.AddRange(CorrectGuesses);
+
+            LettersAvailable = allLetters.Where(x =>
+                    !PreviousGuesses.Contains(x)
+                ).ToList(); // LINQ magic
+        }
+    }
+
+    [Required]
+    public char? Guess { get; set; }
+    public List<char> PreviousGuesses { get; set; } = new List<char>();
+
+    public List<char> LettersAvailable { get; }
+    public List<char> CorrectGuesses { get; } = new List<char>();
+    public List<char> IncorrectGuesses { get; } = new List<char>();
+
+    public bool HasWon { get; }
+    public bool HasLost { get; }
+    public int LivesRemaining { get; }
+
+    public String OutputAnswer()
+    {
+        var answerBuilder new StringBuilder
+    }
+}
