c4b5a063492d81817b67abb1d3d4e4a65b2cf356
David Martin <David.Martin@bglgroup.co.uk>
2019-12-18 13:42:28 +0000

    new commit

--- HangManGame/Controllers/GameController.cs
+++ HangManGame/Controllers/GameController.cs
@@ -42,7 +42,7 @@ namespace HangManGame.Controllers
         public IActionResult SubmitGuess(Game game)
         {
             // Add logic so it only does this when the guess is correct
-            game.CorrectlyGuessed.Add(game.Guess);
+            //game.CorrectlyGuessed.Add(game.Guess);
             game.GetWord("easy");
 
             return RedirectToAction("InPlay", game);  /// was redirect to action 
